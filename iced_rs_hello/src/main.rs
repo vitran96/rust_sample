caea5e63b96faf13d6ec3e42b6f8324f04ab2cc1
vitran96 <tranuytrieuvi@gmail.com>
2023-12-15 08:24:37 +0700

    feature - add hello world

--- iced_rs_hello/src/main.rs
+++ iced_rs_hello/src/main.rs
@@ -1,6 +1,34 @@
 use std::error::Error;
 
-pub fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
-    Ok(())
+use iced::{executor, Element, Settings};
+
+use iced::{Application, Command, Theme};
+
+pub fn main() -> iced::Result {
+    App::run(Settings::default())
+}
+
+struct App {}
+
+impl Application for App {
+    type Executor = executor::Default;
+    type Flags = ();
+    type Message = ();
+    type Theme = Theme;
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        (Self {}, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Title")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        "Hello, world!".into()
+    }
 }
